1751b51d9967132e6c37c7c05f50cbc7b754c7ac
Sheily <SheilyGomez>
2025-05-05 22:59:20 -0600

    Vista del cliente terminada

--- Examen2_ds411/Controllers/ClienteController.cs
+++ Examen2_ds411/Controllers/ClienteController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using static Examen2_ds411.Controllers.ClienteController;
 
 namespace Examen2_ds411.Controllers
 {
@@ -14,14 +15,18 @@ namespace Examen2_ds411.Controllers
         
         public ActionResult EstadoCuenta()
         {
+
+            if (TempData["msj"] != null)
+            {
+                ViewBag.msjT = TempData["msj"];
+            }
+
             if (System.Web.HttpContext.Current.Session["rol"] == null)
             {
                 return RedirectToAction("Login", "Login");
             }
             else
             {   
-                //Si es admin lo mando a la vista de Admin
-                //Lo cambias por la que sera tu vista principal
 
                 if (System.Web.HttpContext.Current.Session["rol"]?.ToString() == "administrador")
                 {
@@ -35,6 +40,105 @@ namespace Examen2_ds411.Controllers
             }
 
         }
+        [HttpGet]
+        public ActionResult TransaccionCliente(string tipo, string n)
+        {
+            if(Session["rol"] == null)
+                return RedirectToAction("Login","Login");
+
+            if (Session["rol"]?.ToString() == "administrador")
+                return RedirectToAction("Cuenta", "Admin");
+
+            
+
+            int ncta = int.Parse(n);
+            //cuenta ct = contexto.cuenta.FirstOrDefault(c => c.ncta == ncta);
+            int cant_t = contexto.transacciones.Count();
+            
+            TempData["Tipo"] = tipo;
+            TempData["ncta"] = n;
+            TempData["cod_transac"] = cant_t+1;
+            
+            return View();
+
+        }
+
+        [HttpPost]
+        public ActionResult GuardarTransaccion(transacciones t)
+        {
+            if (Session["rol"] == null)
+                return RedirectToAction("Login", "Login");
+
+            if (Session["rol"]?.ToString() == "administrador")
+                return RedirectToAction("Cuenta", "Admin");
+
+
+
+            int cod_cliente = int.Parse(System.Web.HttpContext.Current.Session["cod_cliente"].ToString());
+            cliente cl = contexto.cliente.FirstOrDefault(c => c.cod_cliente == cod_cliente);
+            
+            cuenta ct = contexto.cuenta.FirstOrDefault(c => c.ncta == t.ncta && c.cod_cliente == cod_cliente);
+
+            if (ct == null)
+            {
+                TempData["mjs"] = "ErrorCuenta";
+                return RedirectToAction("EstadoCuenta");
+            }
+
+            DateTime today = DateTime.Today;
+            DateTime tomorrow = today.AddDays(1);
+
+          
+            int transaccionesHoy = contexto.transacciones
+                .Where(t_db => t_db.fecha.HasValue && 
+                               t_db.fecha.Value >= today &&
+                               t_db.fecha.Value < tomorrow) 
+
+                .Join(contexto.cuenta, 
+                      t_db => t_db.ncta, 
+                      c_db => c_db.ncta, 
+                      (t_db, c_db) => new { Transaction = t_db, Cuenta = c_db }) 
+                .Where(joined => joined.Cuenta.cod_cliente == cod_cliente) 
+                .Count();
+
+            int limiteDiario = 10;
+
+            if (transaccionesHoy >= limiteDiario)
+            {
+                TempData["msj"] = "limiteDiario";
+                return RedirectToAction("EstadoCuenta"); 
+            }
+
+            t.fecha = DateTime.Now; 
+
+
+            if (t.tipo == "deposito")
+            {
+                contexto.transacciones.Add(t);
+                ct.saldo += t.monto;
+                contexto.SaveChanges();
+                TempData["msj"] = "deposito";
+
+
+            }
+            if (t.tipo == "retiro")
+            {
+                if (ct.saldo == 0 || ct.saldo <t.monto)
+                {
+                    TempData["msj"] = "SaldoIn";
+                }
+                else
+                {
+                    contexto.transacciones.Add(t);
+                    ct.saldo -= t.monto;
+                    contexto.SaveChanges();
+                    TempData["msj"] = "retiro";
+
+                }
+
+            }
+            return RedirectToAction("EstadoCuenta");
+        }
 
         public JsonResult GetEstadoCuenta()
         {
@@ -44,59 +148,59 @@ namespace Examen2_ds411.Controllers
 
             int cod_cliente = int.Parse(System.Web.HttpContext.Current.Session["cod_cliente"].ToString());
 
-            var cuenta = contexto.cuenta.FirstOrDefault(c => c.cod_cliente == cod_cliente);
             var cliente = contexto.cliente.FirstOrDefault(c => c.cod_cliente == cod_cliente);
 
+            var cuentas = contexto.cuenta
+               .Where(c => c.cod_cliente == cod_cliente)
+               .Select(c => new CuentaCliente
+               {
+                   saldo = c.saldo.Value,
+                   numeroCuenta = c.ncta,
+                   Transacciones = contexto.transacciones
+                       .Where(t => t.ncta == c.ncta)
+                       .Select(t => new Transaccion
+                       {
+                           tipo = t.tipo,
+                           monto = t.monto.Value,
+                           fecha = t.fecha
+                       }).ToList()
+               }).ToList();
 
-            if (cuenta == null || cliente == null)
+            if ( cuentas == null || cliente == null)
             {
-                return Json(new { error = "Cuenta o cliente no encontrado." }, JsonRequestBehavior.AllowGet);
+                return Json(new { error = "Ocurio un error" }, JsonRequestBehavior.AllowGet);
             }
 
-            var transacciones = contexto.transacciones
-                .Where(t => t.ncta == cuenta.ncta)
-                .Select(t => new Transaccion
-                {
-                    tipo = t.tipo,
-                    monto = t.monto.Value,
-                    fecha = t.fecha.Value
-
-                }).ToList();
 
             EstadoDeCuenta estado = new EstadoDeCuenta
             {
                 nombreCliente = cliente.nombre_cliente,
                 cod_cliente = cliente.cod_cliente,
                 nit = cliente.nit,
-                numeroCuenta = cuenta.ncta,
-                saldo = cuenta.saldo.Value,
-                Transacciones = transacciones
+                Cuentas = cuentas
             };
 
             return Json(new { resultado = estado }, JsonRequestBehavior.AllowGet);
         }
-
-        //Estas estructuras las voy a modificar con la logica que un usuario puede
-        //tener varias cuentas, pero te pueden servir de referencia
-
-        //Estructuras: 
+ 
         public class Transaccion
         {
             public string tipo { get; set; }
             public double monto { get; set; }
-            public DateTime fecha { get; set; }
+            public DateTime? fecha { get; set; }
+        }
+        public class CuentaCliente {
+            public double saldo { get; set; }
+            public int numeroCuenta { get; set; }
+            public List<Transaccion> Transacciones { get; set; }
         }
 
         public class EstadoDeCuenta
         {
-
             public string nombreCliente { get; set; }
             public int cod_cliente { get; set; }
             public string nit { get; set; }
-            public int numeroCuenta { get; set; }
-            public double saldo { get; set; }
-
-            public List<Transaccion> Transacciones { get; set; }
+            public List<CuentaCliente> Cuentas{ get; set; }
 
         }
 
